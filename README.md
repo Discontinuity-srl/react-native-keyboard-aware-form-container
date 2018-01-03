605f40eab1bfd17d2c01401d3745a47982540ca4
Benetton Roberto <robertobenetton@gmail.com>
2018-01-03 17:02:15 +0100

    added implementation, edited readme

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # react-native-keyboard-aware-form
+
 React Native Form which prevents inputs from being hidden by the keyboard in scroll views, and manage tapping outside fields - iOS and Android
+
+Features:
+
+* The keyboard is dismissed when user tap out of the inputs.
+* The scroll to reveal an input is performed only if the input would be hidden by the keyboard.
+* The scoll is not restored to any prosition after the user dismisses the keyboard.
+* Simple interface, generic implementation.
+
+## Installation
+
+```
+npm install react-native-keyboard-aware-form
+```
+
+## Usage
+
+```js
+export class Form extends React.Component {
+  import { ScrollView } from "react-native"
+  import KeyboardAwareForm from "@discontinuity/react-native-keyboard-aware-form"
+
+
+  render() {
+    const { children } = this.props
+
+    return (
+      <KeyboardAwareForm containerStyle={styles.keyboardAwareFormStyle}>
+         {children}
+      </KeyboardAwareForm>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  keyboardAwareFormStyle: {
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+  },
+})
+```
+
+## Props
+
+| Name           | Description              |
+| -------------- | ------------------------ |
+| children       | The children component   |
+| containerStyle | The childContainer style |
+
+## Credits
+
+Copyright (c) 2017 Discontinuity s.r.l.
+Available under the MIT license.
+
+This component is build up the [react-native-scroll-view-keyboard-manager](https://github.com/Discontinuity-srl/react-native-scroll-view-keyboard-manager)
---
+++ index.js
@@ -0,0 +1,77 @@
+import React from "react"
+import PropTypes from "prop-types"
+import {
+  View,
+  TouchableWithoutFeedback,
+  ScrollView,
+  Keyboard,
+  Platform,
+} from "react-native"
+import AndroidKeyboardAdjust from "react-native-android-keyboard-adjust"
+
+import ScrollViewKeyboardManager, {
+  ANDROID_SOFT_INPUT_MODES,
+} from "@discontinuity/react-native-scroll-view-keyboard-manager"
+
+// ----------------------------------------------------------------------------
+
+const IS_ANDROID = Platform.OS === "android"
+
+// ----------------------------------------------------------------------------
+
+export class KeyboardAwareForm extends React.Component {
+  static propTypes = {
+    children: PropTypes.any.isRequired,
+    containerStyle: PropTypes.number,
+  }
+
+  // -------------------------------------
+
+  componentDidMount() {
+    if (IS_ANDROID) {
+      AndroidKeyboardAdjust.setAdjustResize()
+    }
+  }
+
+  componentWillUnmount() {
+    if (IS_ANDROID) {
+      AndroidKeyboardAdjust.setAdjustPan()
+    }
+  }
+
+  // -------------------------------------
+
+  dismissKeyboard = () => {
+    Keyboard.dismiss()
+  }
+
+  // -------------------------------------
+
+  render() {
+    const { children, containerStyle } = this.props
+    return (
+      <ScrollViewKeyboardManager
+        additionalScrollOffset={20}
+        androidSoftInputMode={ANDROID_SOFT_INPUT_MODES.adjustResize}
+        render={scrollViewProps => {
+          return (
+            <ScrollView
+              {...scrollViewProps}
+              keyboardShouldPersistTaps="always"
+              showsVerticalScrollIndicator={true}
+              keyboardDismissMode="interactive"
+            >
+              <TouchableWithoutFeedback onPress={this.dismissKeyboard}>
+                <View style={containerStyle}>{children}</View>
+              </TouchableWithoutFeedback>
+            </ScrollView>
+          )
+        }}
+      />
+    )
+  }
+}
+
+// ----------------------------------------------------------------------------
+
+export default KeyboardAwareForm
